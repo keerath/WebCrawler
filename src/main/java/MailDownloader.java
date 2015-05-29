d5a60f4ab0bec8ef867e9ca4b3147ca3601293c6
Keerath Jaggi <keerath.jaggi@gmail.com>
2015-05-29 18:38:26 +0530

    able to retrieve mail content

--- src/main/java/MailDownloader.java
+++ src/main/java/MailDownloader.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
@@ -15,21 +17,18 @@ import com.gargoylesoftware.htmlunit.html.HtmlTableRow;
 public class MailDownloader {
 	
 	
-	public void findAnchors(String url) throws FailingHttpStatusCodeException, MalformedURLException, IOException
+	public List<String> getMailContent(String url) throws FailingHttpStatusCodeException, MalformedURLException, IOException
 	{
 		WebClient webClient = new WebClient();
 		HtmlPage currentPage = webClient.getPage(url);
 		List<HtmlAnchor> anchors = currentPage.getAnchors().stream().filter(anchor -> anchor.getHrefAttribute().charAt(0) == '%').collect(Collectors.toList());
-		
+		List<String> content = new LinkedList<String>();
 		for(HtmlAnchor anchor : anchors)
 		{
 			currentPage = anchor.click();
-			HtmlTable table = currentPage.getHtmlElementById("msgview");
-			String content = currentPage.getBody().asText();
-			System.out.println(content);
-			break;
+			content.add(currentPage.getElementsByTagName("pre").get(0).asText());
 		}
 		webClient.close();
-		
+		return content;
 	}
 }
